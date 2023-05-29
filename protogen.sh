91a198d0feedd9c3892e7f1e5bcee2951b8abc58
Anvar Abdulsatarov <anvarabdulsatarov@gmail.com>
2023-05-29 17:07:22 +0500

    feat: protogen variables

--- protogen.sh
+++ protogen.sh
@@ -3,12 +3,20 @@ mkdir "protogen"
 
 echo $OSTYPE
 
+PROTO_PATHS=(
+  ./protos/common/*.proto 
+  ./protos/sample/*.proto 
+  ./protos/broker/user/*.proto 
+  ./protos/broker/sample/*.proto
+)
 
 # For windows and linux based OS, there are different methods for protogen
 if [[ "$OSTYPE" == "msys" ]]; then
-  protoc -I ./protos --plugin=protoc-gen-ts_proto=".\node_modules\.bin\protoc-gen-ts_proto.cmd" --ts_proto_out=./protogen ./protos/common/*.proto ./protos/sample/*.proto ./protos/broker/user/*.proto ./protos/broker/sample/*.proto
+  protoc -I ./protos --plugin=protoc-gen-ts_proto=".\node_modules\.bin\protoc-gen-ts_proto.cmd" --ts_proto_out=./protogen "${PROTO_PATHS[@]}"
 else
-  protoc -I ./protos --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./protogen ./protos/common/*.proto ./protos/sample/*.proto ./protos/broker/user/*.proto ./protos/broker/sample/*.proto
+  protoc -I ./protos --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./protogen ./protos/common/*.proto "${PROTO_PATHS[@]}"
 fi;
 
 sleep 3
+
+
